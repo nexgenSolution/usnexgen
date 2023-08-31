ffea56e2a1e87bb447c4fc614da9f42203a6f7ec
minhldnex <minh.ly@usnexgen.com>
2023-08-31 16:49:36 +0700

    cap nhat

--- js/script.js
+++ js/script.js
@@ -1,45 +1,50 @@
 //Vietnamese
 $(function() {
     $(".vi").click(function() {
+        $("#mainNav li:nth-child(1) a").text("Trang chủ");
+        $("#mainNav li:nth-child(2) a").text("Giới thiệu");
+        $("#mainNav li:nth-child(3) a").text("Sản phẩm");
+        $("#call1").text("GỌI CHO CHÚNG TÔI NGAY");
+        $("#menudropdown ul:nth-child(2) li:nth-child(1) a").text("Công nghệ");
+        $("#menudropdown ul:nth-child(2) li:nth-child(2) a").text("Vật lý");
+        $("#menudropdown ul:nth-child(2) li:nth-child(3) a").text("Hóa học");
+        $("#menudropdown ul:nth-child(2) li:nth-child(4) a").text("Sinh học");
         $("#menuleft").children().eq(0).text("Tất cả các công nghệ");
         $("#submenuleft").children().eq(0).text("Kỹ thuật điện tử");
-        $("#submenuleft").children().eq(1).text("Abc");
-        $("#submenuleft").children().eq(2).text("xyz");
-        $("#submenuleft").children().eq(3).text("aesfdgsdfg");
-        $("#submenuleft").children().eq(4).text("awsdvsadfv");
-        $("#submenuleft").children().eq(5).text("asdvad");
-    
+        $("#submenuleft").children().eq(1).text("Công nghệ ô tô");
+        $("#submenuleft").children().eq(2).text("Chương trình đào tạo COM4LAB");
+        $("#submenuleft").children().eq(3).text("Năng lượng tái tạo");
+        $("#submenuleft").children().eq(4).text("Phương pháp học");
+        $("#submenuleft").children().eq(5).text("Thiết bị đo và phân tích năng lương (CASSY)");
+        $("#c7357 p:nth-child(1) b").text("Khám phá danh mục hệ thống đào tạo toàn diện của chúng tôi với các giải pháp của các thương hiệu công nghệ LEYBOLD, FEEDBACK và ELWE trong các lĩnh vực:");
+        $("#c7357 ul:nth-child(1) li:nth-child(1) ").text("minh");
+        $(".language-selected .en").css({"-webkit-filter": "grayscale(100%)","filter": "grayscale(100%)"});
+        $(".language-selected .vi").css({"-webkit-filter": "grayscale(0%)","filter": "grayscale(0%)"});
+        
 
-        // //header
-        // $(".nav-menu").children().eq(0).text("Solutions");
-        // $(".nav-menu").children().eq(1).text("community");
-        // $(".nav-menu").children().eq(2).text("Buy");
-        // $(".nav-menu").children().eq(3).text("Log in");
-        // $(".language-selected").text("en-US");
-        // $(".language-selected").removeClass("change-es");
-        // $(".language-selected").removeClass("change-br");
-        // $(".language-selected").addClass("change-en");
-        // //section
-        // $("#title").text("A simple webpage with Milti-language option");
-        // //About
-        // $(".about-container ul:nth-child(1) li:nth-child(1) a").text("Help Center");
-        // $(".about-container ul:nth-child(1) li:nth-child(2) a").text("Ethics Line");
-        // $(".about-container ul:nth-child(1) li:nth-child(3) a").text("Payments");
-        // $(".about-container ul:nth-child(2) li:nth-child(1) a").text("Support");
-        // $(".about-container ul:nth-child(2) li:nth-child(2) a").text("Help Center");
-        // $(".about-container ul:nth-child(2) li:nth-child(3) a").text("Terms and Policies");
+       
     });
 });
 
 //English
 $(function() {
     $(".en").click(function() {
+        $("#mainNav li:nth-child(1) a").text("Home");
+        $("#mainNav li:nth-child(2) a").text("About");
+        $("#mainNav li:nth-child(3) a").text("Products");
+        $("#mainNav li:nth-child(4) a").text("CALL US NOW");
+        $("#menudropdown ul:nth-child(2) li:nth-child(1) a").text("Technology");
+        $("#menudropdown ul:nth-child(2) li:nth-child(2) a").text("Physics");
+        $("#menudropdown ul:nth-child(2) li:nth-child(3) a").text("Chemistry");
+        $("#menudropdown ul:nth-child(2) li:nth-child(4) a").text("Biology");
         $("#menuleft").children().eq(0).text("All Technologies");
         $("#submenuleft").children().eq(0).text("Electrical Engineering");
-        $("#submenuleft").children().eq(1).text("Abc");
-        $("#submenuleft").children().eq(2).text("xyz");
-        $("#submenuleft").children().eq(3).text("aesfdgsdfg");
-        $("#submenuleft").children().eq(4).text("awsdvsadfv");
-        $("#submenuleft").children().eq(5).text("asdvad");
+        $("#submenuleft").children().eq(1).text("Automotive Technology");
+        $("#submenuleft").children().eq(2).text("COM4LAB");
+        $("#submenuleft").children().eq(3).text("Regenerative Energy");
+        $("#submenuleft").children().eq(4).text("Learning Methods");
+        $("#submenuleft").children().eq(5).text("Power Analyser CASSY");
+        $(".language-selected .vi").css({"-webkit-filter": "grayscale(100%)","filter": "grayscale(100%)"});
+        $(".language-selected .en").css({"-webkit-filter": "grayscale(0%)","filter": "grayscale(0%)"});
     });
 });
